7b487c506e77c28042f5452b81748f1b3e9ee844
Julien PERRIER <julien.perrier@sii.fr>
2023-08-08 14:24:09 +0200

    calendar Page

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import FullCalendar, { formatDate } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
+import { formatDate } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -22,7 +23,7 @@ const Calendar = () => {
 
   const handleDateClick = (selected) => {
     const title = prompt("Please enter a new title for your event");
-    const calendarApi = selected.view.clendar;
+    const calendarApi = selected.view.calendar;
     calendarApi.unselect();
 
     if (title) {
@@ -31,7 +32,7 @@ const Calendar = () => {
         title,
         start: selected.startStr,
         end: selected.endStr,
-        allDta: selected.allday,
+        allDay: selected.allDay,
       });
     }
   };
@@ -48,17 +49,81 @@ const Calendar = () => {
 
   return (
     <Box m="20px">
-      <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
-      <Box display="flex" justifyContent="space-between"></Box>
-      {/* Calendar Sidebar */}
-      <Box
-        flex="1 1 20%"
-        backgroundColor={colors.primary[400]}
-        p="15px"
-        borderRadius="4px"
-      >
-        {" "}
-        <Typography variant="h5">Events</Typography>
+      <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+
+      <Box display="flex" justifyContent="space-between">
+        {/* CALENDAR SIDEBAR */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        {/* CALENDAR */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "12315",
+                title: "All-day event",
+                date: "2022-09-14",
+              },
+              {
+                id: "5123",
+                title: "Timed event",
+                date: "2022-09-28",
+              },
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   );
